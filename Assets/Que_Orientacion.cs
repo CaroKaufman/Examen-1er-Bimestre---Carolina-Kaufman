e721b078d139c83bbe9d223d6ed21631cc75cb7e
CaroKaufman <101335676+CaroKaufman@users.noreply.github.com>
2022-04-11 11:43:08 -0300

    ñ

--- Assets/Que_Orientacion.cs
+++ Assets/Que_Orientacion.cs
@@ -19,7 +19,7 @@ public class Que_Orientacion : MonoBehaviour
     
         }
 
-        else if (anio < 1 || anio > 5)
+         if (anio < 1 || anio > 5)
         {
             Debug.Log("Error. Ingrese números entre 1 y 5");
         }
@@ -28,7 +28,7 @@ public class Que_Orientacion : MonoBehaviour
 
        // Si se ingresa un valor diferente a las opciones válidas en el campo ingreso de orientación escribir en consola 
        //“Solo puede ingresar T, D, G, M o H”.
-        else if (orientacion != "T" && orientacion != "D" && orientacion != "G" && orientacion != "M" && orientacion != "H")
+         if (orientacion != "T" && orientacion != "D" && orientacion != "G" && orientacion != "M" && orientacion != "H")
         {
             Debug.Log("Solo puede ingresar T, D, G, M o H");
         }
