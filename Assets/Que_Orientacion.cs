f6504119034747c2c659c37a3b3a8439c2565ebe
CaroKaufman <101335676+CaroKaufman@users.noreply.github.com>
2022-04-11 11:33:18 -0300

    Termine
    yay

--- Assets/Que_Orientacion.cs
+++ Assets/Que_Orientacion.cs
@@ -28,7 +28,7 @@ public class Que_Orientacion : MonoBehaviour
 
        // Si se ingresa un valor diferente a las opciones válidas en el campo ingreso de orientación escribir en consola 
        //“Solo puede ingresar T, D, G, M o H”.
-        else if (orientacion != "T" ||orientacion != "D" || orientacion != "G" || orientacion != "M"|| orientacion != "H")
+        else if (orientacion != "T" && orientacion != "D" && orientacion != "G" && orientacion != "M" && orientacion != "H")
         {
             Debug.Log("Solo puede ingresar T, D, G, M o H");
         }
